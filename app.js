54df1bfa5bdaf146d96e67942224bc445706ce18
NickoNO242 <nico568_97@hotmail.com>
2021-10-03 17:18:55 -0500

    correccion en PORT ||

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"))})
- app.listen(3000, () => console.log("listen in port 3000"))
+ app.listen(process.env.PORT || 3000, () => console.log("listen in port"))
